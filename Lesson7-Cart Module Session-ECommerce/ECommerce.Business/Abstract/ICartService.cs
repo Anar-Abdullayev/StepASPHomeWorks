a92d9dddce06d736dc6a45cdf79394eab7ae1f2e
Anar Abdullayev <anar.abdlyv@gmail.com>
2025-02-04 17:54:35 +0400

    Lesson 7-Homework added

---
+++ Lesson7-Cart Module Session-ECommerce/ECommerce.Business/Abstract/ICartService.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.Entities.Concrete;
+using ECommerce.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface ICartService
+    {
+        void AddToCart(Cart cart, Product product);
+        List<CartLine>? List(Cart cart);
+        void RemoveFromCart(Cart cart, int productId);
+        void Decrease(Cart cart, int productId);
+        void Increase(Cart cart, int productId);
+    }
+}
---
+++ Lesson7-Cart Module Session-ECommerce/ECommerce.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<List<Product>> GetAllByCategoryAsync(int categoryId);
+        Task<Product> GetByIdAsync(int id);
+    }
+}
